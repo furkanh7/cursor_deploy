ab7b38ec4dd50863e2d08a43401329380f906ed0
furkanh7 <furkanhilaloglu9@gmail.com>
2024-12-26 16:53:00 +0300

    Fix EmailJS import in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './i18n';
+import emailjs from '@emailjs/browser';
+
+emailjs.init('ipF6aclGp6BxGrKjK');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
